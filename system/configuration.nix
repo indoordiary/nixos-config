d3e1445c527b2f028bff57ec0d39cfdaadbc3638
indoordiary <lfn2333333@gmail.com>
2025-04-08 08:27:33 +0800

    Update configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -117,7 +117,7 @@ in
   users.users.chenhsi = {
     isNormalUser = true;
     descriptions = "Chen Hsi";
-    hashedPassword = "$6$8/ZJ0jr/BWf4at92$PmRuEML2eiCHcRe/vuc54xgSkU.T7bO6ljUqczhkpw3kAv1mTl.PLMOhRuG.yIiEklJGwcHq3szit4PPkZaEM0";
+    initalPassword = "123456";
   };
 
 }
